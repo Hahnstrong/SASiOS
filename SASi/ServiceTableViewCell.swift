d7e78086f16d754ba94afcfcb4af91315a1d0f09
Caleb Strong <hahnstrong@gmail.com>
2017-08-29 14:12:30 -0600

    Almost all network callers completle. All button funcionality complete. Service ordering via email is complete

---
+++ SASi/ServiceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ServiceTableViewCell.swift
+//  SASi
+//
+//  Created by Caleb Strong on 8/29/17.
+//  Copyright © 2017 Caleb Strong. All rights reserved.
+//
+
+import UIKit
+
+class ServiceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
